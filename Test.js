783620df2bde7ae0f174a6495ed63d7b54effe3a
Muthumahesh K <muthumahesh.k@ivygdcad.ivy-support.com>
2022-12-08 11:41:47 +0530

    changeed

--- Test.js
+++ Test.js
@@ -1,5 +1,5 @@
 let a=24;
-let b=543;
+let b=54;
 c=a+b;
 console.log(a + ' and ' + b + ' values ');
-console.log('Addition value of ' + a + ' and ' + b + ' is ' + c);
+console.log('Multiplication value of ' + a + ' and ' + b + ' is ' + c);
